a7a793208bd9acdb7949a17b38814a7efce5ef86
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-03-04 22:22:50 +0200

    mobile menu fonts adjusted for width 320px, clients mobile grid remade due to horizontal scroll appearing

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,17 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileBtnClose = document.querySelector("[data-menu-close]");
-  
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+
   menuBtnRef.addEventListener("click", () => {
-      document.body.classList.toggle('modal-open')
-      
-      mobileMenuRef.classList.toggle("is-open");
-    })
+    document.body.classList.toggle('modal-open');
+
+    mobileMenuRef.classList.toggle("is-open");
+  })
 
   mobileBtnClose.addEventListener('click', () => {
-      document.body.classList.toggle('modal-open')
-      
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })()
+    document.body.classList.toggle('modal-open');
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})()
